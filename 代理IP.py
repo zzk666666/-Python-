c1c30420967f0cc5234f9b3161bd68d949a2f7f7
Gewöhnlicher Programmierer <86613239+zzk666666@users.noreply.github.com>
2022-07-19 20:17:36 +0800

    Update 代理IP.py

--- 代理IP.py
+++ 代理IP.py
@@ -5,7 +5,7 @@ class Baidu:
     def __init__(self):
         self.url_="https://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&dyTabStr=MCwzLDIsNiwxLDUsNCw4LDcsOQ%3D%3D&word=%E5%92%92%E6%80%A8"
         self.num_=0
-    @retry(stop_max_attempt_number=3)
+    @retry(stop_max_attempt_number=3) #最大重试次数
     def send_requset(self):
         self.num_+=1
         print(self.num_)
@@ -18,7 +18,7 @@ class Baidu:
 if __name__=="__main__":
     url_="https://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&dyTabStr=MCwzLDIsNiwxLDUsNCw4LDcsOQ%3D%3D&word=%E5%92%92%E6%80%A8"
     headers_={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
-    proxies_={"http":"http://192.168.1.10:54396"}
+    proxies_={"http":"http://192.168.1.10:54396"} #ip地址与端口
     response_=requests.get(url_,headers=headers_,proxies=proxies_,timeout=3)
     str_data=response_.content.decode()
     print(str_data)
