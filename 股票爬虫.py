e3b2d64dba212f5fd637efce08c344a670b8f380
Gewöhnlicher Programmierer <86613239+zzk666666@users.noreply.github.com>
2022-10-23 02:27:07 +0800

    Add files via upload

--- 股票爬虫.py
+++ 股票爬虫.py
@@ -1,7 +1,7 @@
 import requests
 from pyecharts.charts import Bar
 import pyecharts.options as opts
-import re
+import re,time
 if __name__ == '__main__':
     url_="http://65.push2.eastmoney.com/api/qt/clist/get?cb=jQuery1124024051600773740023_1662100517929&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f3&fs=b:MK0144&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f26,f22,f11,f62,f128,f136,f115,f152&_=1662100517930"
 
@@ -33,24 +33,35 @@ if __name__ == '__main__':
     res2 = re.findall('"f2":\d+\D\d+', r)
     res3 = re.findall('"f3":\d+\D\d+', r)
     name = []
+    name2=[]
     newprice = []
+    newprice2=[]
     todaychg=[]
+    result=[]
     print("名称 最新价 今日涨跌幅")
     for i in range(len(res)):
         name.append(res[i])
         np=re.search('\d+\D\d+', res2[i]).group()
         tc = re.search('\d+\D\d+', res3[i]).group()
         newprice.append(np)
+        todaychg.append(tc)
         print(res[i]+":"+np+"\t"+tc+"%")
 print(res)
 print(newprice)
+spc1 = time.strftime("%Y-%m-%d", time.localtime()) + '股价图.html'
+spc2 = time.strftime("%Y-%m-%d", time.localtime()) + '涨跌幅图(%).html'
+
+
+
+
 bar = Bar()
-bar.add_xaxis(res)
-bar.add_yaxis("股价图",newprice)
+bar.add_xaxis(name)
+bar.add_yaxis(spc1.replace(".html",""),newprice,itemstyle_opts=opts.ItemStyleOpts(color='blue'))
+bar.add_yaxis(spc2.replace(".html",""),todaychg,itemstyle_opts=opts.ItemStyleOpts(color='red'))
 bar.set_global_opts(
     xaxis_opts=opts.AxisOpts(
         axislabel_opts=opts.LabelOpts(rotate=35),
     ),
     yaxis_opts=opts.AxisOpts(name="价格：（元/股）")
 )
-bar.render("股价图.html")
+bar.render(str(spc1))
